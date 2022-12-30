9a8b55a7431e9360404bde25b596619583f0e48d
Agustin Coelho <coelhoagustin1@gmail.com>
2022-12-30 01:50:15 -0300

    fixes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
-import express, {Response} from 'express'
+import express, { Response } from 'express'
 import UserController, { IUserController } from '../controllers/user'
 import passport from '../middlewares/passport'
 import { ensureToken } from '../middlewares/jwt'
@@ -6,7 +6,7 @@ import { ensureToken } from '../middlewares/jwt'
 const router = express.Router()
 const controller: IUserController = new UserController()
 
-router.get('/', (_, res: Response) => res.json({data: 'Api is working!'}))
+router.get('/', (_, res: Response) => res.json({ data: 'Api is working!' }))
 router.post('/api/register', passport.authenticate('register'), controller.register)
 router.post('/api/login', passport.authenticate('login'), controller.login)
 router.get('/api/users', ensureToken, controller.getUsers)
